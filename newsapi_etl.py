dc6e7b1736a3514ea7738eb096a4fc7c482986a7
Akash Raj <rajakash31@gmail.com>
2024-02-27 09:38:17 -0500

    newsapi call works

--- newsapi_etl.py
+++ newsapi_etl.py
@@ -3,14 +3,14 @@ from newsapi import NewsApiClient
 # Init
 newsapi = NewsApiClient(api_key='4f7a2da18c2a441da4b0bb13f3e4cce5')
 
-# # /v2/top-headlines
+# /v2/top-headlines
 # top_headlines = newsapi.get_top_headlines(q='bitcoin',
 #                                           sources='bbc-news,the-verge',
 #                                           category='business',
 #                                           language='en',
 #                                           country='us')
 
-# # /v2/everything
+# /v2/everything
 # all_articles = newsapi.get_everything(q='bitcoin',
 #                                       sources='bbc-news,the-verge',
 #                                       domains='bbc.co.uk,techcrunch.com',
@@ -22,3 +22,4 @@ newsapi = NewsApiClient(api_key='4f7a2da18c2a441da4b0bb13f3e4cce5')
 
 # /v2/top-headlines/sources
 sources = newsapi.get_sources()
+print(sources)
