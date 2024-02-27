00e4e587e6d978990b26e8dc4b59c90c23850090
Akash Raj <rajakash31@gmail.com>
2024-02-27 13:08:09 -0500

    initial etl pipeline

--- newsapi_etl.py
+++ newsapi_etl.py
@@ -1,25 +1,31 @@
 from newsapi import NewsApiClient
+from datetime import datetime
+import pandas as pd
 
 # Init
 newsapi = NewsApiClient(api_key='4f7a2da18c2a441da4b0bb13f3e4cce5')
 
 # /v2/top-headlines
-# top_headlines = newsapi.get_top_headlines(q='bitcoin',
-#                                           sources='bbc-news,the-verge',
+# top_headlines = newsapi.get_top_headlines(q='',
+#                                           sources='abc-news',
 #                                           category='business',
 #                                           language='en',
-#                                           country='us')
-
-# /v2/everything
-# all_articles = newsapi.get_everything(q='bitcoin',
-#                                       sources='bbc-news,the-verge',
-#                                       domains='bbc.co.uk,techcrunch.com',
-#                                       from_param='2017-12-01',
-#                                       to='2017-12-12',
-#                                       language='en',
-#                                       sort_by='relevancy',
-#                                       page=2)
-
+#                                           country='US')
 # /v2/top-headlines/sources
-sources = newsapi.get_sources()
-print(sources)
+# sources = newsapi.get_sources()
+
+
+# /v2/everything - extract all article in a specific time range
+all_articles = newsapi.get_everything(q='',
+                                      sources='abc-news',
+                                      domains='abcnews.go.com',
+                                      from_param='2024-02-05',
+                                      to='2024-02-20',
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=1)
+
+
+articles = all_articles['articles']
+df_articles = pd.json_normalize(articles)
+print(df_articles.head(2))
